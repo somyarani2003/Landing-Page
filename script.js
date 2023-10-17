4ddd934c314efeb62eaecf071080bcab402d53c3
Somya Rani <139989084+somyarani2003@users.noreply.github.com>
2023-10-17 20:06:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// let navbar = document.querySelector('.header .navbar')
+
+// document.querySelector('#menu').onclick = () => {
+//     navbar.classList.add('.active');
+// }
+// document.querySelector('#close').onclick = () => {
+//     navbar.classList.remove('.active');
+// }
+
+
+
+// mousemove home img
+
+document.addEventListener('mousemove', move);
+
+function move(e) {
+    this.querySelectorAll('.move').forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX * speed) / 120
+        const y = (window.innerWidth - e.pageY * speed) / 120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+
+gsap.from('.logo', { opacity: 0, duration: -2, delay: 2, y: 30 })
+gsap.from('.navbar .nav_item', { opacity: 0, duration: -1, delay: 1.9, y: 20 })
+
+gsap.from('.title', { opacity: 0, duration: -2, delay: 2.6, y: 40 })
+gsap.from('.description', { opacity: 0, duration: -2, delay: 2.8, y: 30 })
+gsap.from('.btn', { opacity: 0, duration: -1, delay: 1.6, y: 30 })
+gsap.from('.image', { opacity: 0, duration: -4, delay: 2.6, y: 30 })
+
+
+gsap.from('.btn1', { opacity: 0, duration: -1, delay: 1.6, y: 30 })
+gsap.from('.btn2', { opacity: 0, duration: -1, delay: 1.6, y: 30 })
